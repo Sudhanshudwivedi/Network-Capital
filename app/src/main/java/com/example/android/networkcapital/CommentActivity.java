ea050f3cf6bc7c7581a2ac3ccb3a15073b715d46
Nimesh <nkumarsachan@gmail.com>
2018-06-23 01:11:53 +0530

    minor changes

--- app/src/main/java/com/example/android/networkcapital/CommentActivity.java
+++ app/src/main/java/com/example/android/networkcapital/CommentActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -112,6 +113,11 @@ public class CommentActivity extends AppCompatActivity {
 
                     }
                 });
+
+                InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                mText.getText().clear();
 
 
 
