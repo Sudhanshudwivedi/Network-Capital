6394de0a759211249cca4e586cf066361ac1f3bf
Nimesh <nkumarsachan@gmail.com>
2018-07-30 17:14:23 +0530

    bug fixes

---
+++ app/src/main/java/com/network/android/networkcapital/Friends.java
@@ -0,0 +1,26 @@
+package com.network.android.networkcapital;
+
+/**
+ * Created by Sudhanshu on 4/21/2018.
+ */
+
+public class Friends {
+
+    public String date;
+
+    public Friends(){
+
+    }
+
+    public Friends(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
